99db4b264811b4433713294e0236ef1918c30dac
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2026-04-30 09:35:49 +0000

    Initial commit

---
+++ ba-king/ba_kingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ba_kingApp.swift
+//  ba-king
+//
+//  Created by wijayarakhmat98 on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ba_kingApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
