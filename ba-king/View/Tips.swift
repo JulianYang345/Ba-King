605458f820f5428b746a13d0327305edba973942
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2026-05-04 06:19:25 +0000

    Create folder structure

---
+++ ba-king/View/Tips.swift
@@ -0,0 +1,18 @@
+//
+//  Tips.swift
+//  ba-king
+//
+//  Created by wijayarakhmat98 on 5/4/26.
+//
+
+import SwiftUI
+
+struct Tips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tips()
+}
