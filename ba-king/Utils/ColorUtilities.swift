98a55ec9412bf3b376ba29a71c25e9dd3cc6f9a3
Joel <joelsinaga2701@gmail.com>
2026-05-05 13:49:44 +0700

    Add Color Extension

---
+++ ba-king/Utils/ColorUtilities.swift
@@ -0,0 +1,35 @@
+//
+//  ColorUtilities.swift
+//  ba-king
+//
+//  Created by Joel Nathanael Marsahala on 04/05/26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+	static let customcolors = CustomColors()
+	
+	struct CustomColors {
+		//tinggal copy sesuai kebutuhan warna
+		
+		let primaryOrangeColor =
+		Color("primary_orange_color")
+		
+		let secondaryGreenColor =
+		Color("secondary_green_color")
+		
+		let buttonsecondaryGreyColor =
+		Color("buttonsecondary_grey_color")
+		
+		let backgroundColor =
+		Color("background_color")
+		
+		let textBrownColor =
+		Color("text_brown_color")
+		
+		let textGreyColor =
+		Color("text_grey_secondary_color")
+	}
+}
